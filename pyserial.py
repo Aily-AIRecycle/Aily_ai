1bbe9c8b3c8ecd62bb88932b9cbd5d0b6a9b6e26
dltkdgns00 <s20604@naver.com>
2023-07-24 19:55:36 +0900

    modified

--- pyserial.py
+++ pyserial.py
@@ -9,14 +9,13 @@ ser2 = serial.Serial("COM7", baudrate=9600) # ser2 = compressor
 
 time.sleep(2)
 
-ser2.write('compress\n'.encode()) # comspressor run
-
-
 if param == '3.0': # general trash
     ser1.write('1\n'.encode()) # rail run
 if param == '1.0' or '2.0': # plastic trash
+    ser2.write('compress\n'.encode()) # comspressor run
     ser1.write('2\n'.encode()) # rail run
 if param == '0.0': # can
+    ser2.write('compress\n'.encode()) # comspressor run
     ser1.write('3\n'.encode()) # rail run
 
 
